6363abea68e8c8fa04e4d236448a320b2cd15e5e
Kenshi <Aaladar@proton.me>
2024-01-11 07:10:59 -0600

    Se agrega un recordatorio por si cambiamos de lugar la carpeta public

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 // Importar la libreria
+// Librerias
 const express = require("express");
 
+// Objetos
 // Objetos para llamar los metodos de express
 const app = express();
 
-// Ruta inicial
-app.get("/", function(req, res) {
-    res.send("Hola estudiantes");
-});
+// // Ruta inicial
+// app.get("/", function(req, res) {
+//     res.send("Hola estudiantes");
+// });
 
 // Ruta de archivos estaticos
+// Middleware
 app.use(express.static("public"));
+// Si no sabemos donde esta public podemos usar la siguiente linea: app.use('/static', express.static(__dirname + '/public'));
+
 
 // Configurar el puerto usado para el servidor local
-app.listen(3000, function() {
+// Puerto o servidor
+pp.listen(3000, function() {
     console.log("El servidor es http://localhost:3000");
 });
